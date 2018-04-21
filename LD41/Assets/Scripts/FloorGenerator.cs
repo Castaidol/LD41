508a57b5443be164d71441f1aa88bd52b00414dd
Castaidol <gabriele.castaway@gmail.com>
2018-04-21 06:59:42 +0200

    Player/Rooms
    basic Player movement
    room generator using maps

---
+++ LD41/Assets/Scripts/FloorGenerator.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorGenerator : MonoBehaviour {
+    public Texture2D map;
+
+    public ColorToPrefab[] colorMappings;
+
+    private float offsetX = 14.5f;
+    private float offsetY = 7.5f;
+
+    void Start()
+    {
+        GenerateRoom();
+    }
+
+    void GenerateRoom()
+    {
+        for (int x = 0; x < map.width; x++)
+        {
+            for (int y = 0; y < map.height; y++)
+            {
+                GenerateTile(x, y);
+            }
+        }
+    }
+
+    void GenerateTile(int x, int y)
+    {
+        Color pixelColor = map.GetPixel(x, y);
+
+        if (pixelColor.a == 0)
+        {
+            return;
+        }
+
+        foreach (ColorToPrefab colorMapping in colorMappings)
+        {
+            if (colorMapping.color.Equals(pixelColor))
+            {
+                Vector2 position = new Vector2(x - offsetX, y - offsetY);
+                Instantiate(colorMapping.prefab, position, Quaternion.identity, transform);
+            }
+        }
+    }
+}
---
+++ LD41/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMovement : MonoBehaviour 
+{
+
+    public float speed;
+
+    private Rigidbody2D rb2D;
+    private Vector2 moveVelocity;
+
+	// Use this for initialization
+	void Start () 
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+	}
+	
+	
+	void Update () 
+    {
+        Vector2 movementInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        moveVelocity = movementInput.normalized * speed;
+	}
+
+	private void FixedUpdate()
+	{
+        rb2D.MovePosition(rb2D.position + moveVelocity * Time.deltaTime);
+	}
+}
