de6a1cb0d5a4149dfb2f75cb61457aaf141b8db6
Castaidol <gabriele.castaway@gmail.com>
2018-04-21 13:06:49 +0200

    Enemy
    Start enemies chasing player

---
+++ LD41/Assets/Scripts/Enemies/ChasePlayer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChasePlayer : MonoBehaviour {
+
+    public Transform target;
+
+    public float speed;
+
+	void Start () 
+    {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+	}
+	
+	void Update () 
+    {
+        transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+	}
+}
